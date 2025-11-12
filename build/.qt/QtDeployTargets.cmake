fed5dabb2295979b08784a89fa33c9569b7a40d6
daixianhao <xianhaodai@gmail.com>
2025-11-12 16:17:40 -0500

    Main Structure of Gomoku and A* search algorithm

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_GomokuQt6_FILE /Users/daixianhao/Downloads/gomoku-qt6-ready/build/GomokuQt6)
+set(__QT_DEPLOY_TARGET_GomokuQt6_TYPE EXECUTABLE)
---
+++ build/CMakeFiles/GomokuQt6.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GomokuQt6_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/GomokuQt6_autogen.dir/ParseCache.txt"
+  "GomokuQt6_autogen"
+  "CMakeFiles/GomokuQt6.dir/GomokuQt6_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/GomokuQt6.dir/GomokuQt6_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/GomokuQt6.dir/src/aiclient.cpp.o"
+  "CMakeFiles/GomokuQt6.dir/src/aiclient.cpp.o.d"
+  "CMakeFiles/GomokuQt6.dir/src/boardwidget.cpp.o"
+  "CMakeFiles/GomokuQt6.dir/src/boardwidget.cpp.o.d"
+  "CMakeFiles/GomokuQt6.dir/src/gamecontroller.cpp.o"
+  "CMakeFiles/GomokuQt6.dir/src/gamecontroller.cpp.o.d"
+  "CMakeFiles/GomokuQt6.dir/src/gamemodel.cpp.o"
+  "CMakeFiles/GomokuQt6.dir/src/gamemodel.cpp.o.d"
+  "CMakeFiles/GomokuQt6.dir/src/main.cpp.o"
+  "CMakeFiles/GomokuQt6.dir/src/main.cpp.o.d"
+  "CMakeFiles/GomokuQt6.dir/src/mainwindow.cpp.o"
+  "CMakeFiles/GomokuQt6.dir/src/mainwindow.cpp.o.d"
+  "CMakeFiles/GomokuQt6.dir/src/python_ai_process.cpp.o"
+  "CMakeFiles/GomokuQt6.dir/src/python_ai_process.cpp.o.d"
+  "GomokuQt6"
+  "GomokuQt6.pdb"
+  "GomokuQt6_autogen/mocs_compilation.cpp"
+  "GomokuQt6_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GomokuQt6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
